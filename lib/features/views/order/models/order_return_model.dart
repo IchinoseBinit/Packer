77700302143de427027a1fda73e74a74e7162887
Sujeet <sujeetchakra69@gmail.com>
2025-07-20 14:37:38 +0545

    changes

--- lib/features/views/order/models/order_return_model.dart
+++ lib/features/views/order/models/order_return_model.dart
@@ -2,13 +2,17 @@ import 'package:packer/controllers/extensions/string_extension.dart';
 
 class OrderReturnModel {
   late final int id;
-  late final  int orderId;
-  late final  String basket;
-  late final  String createdAt;
+  late final int orderId;
+  late final String basket;
+  late final String createdAt;
   late final List<OrderItems> orderItems;
 
   OrderReturnModel(
-      {required this.id, required this.orderId, required this.basket, required this.createdAt, required this.orderItems});
+      {required this.id,
+      required this.orderId,
+      required this.basket,
+      required this.createdAt,
+      required this.orderItems});
 
   OrderReturnModel.fromJson(Map<String, dynamic> json) {
     id = json['id'].toString().toInt();
@@ -18,7 +22,7 @@ class OrderReturnModel {
     if (json['order_items'] != null) {
       orderItems = <OrderItems>[];
       json['order_items'].forEach((v) {
-        orderItems!.add(OrderItems.fromJson(v));
+        orderItems.add(OrderItems.fromJson(v));
       });
     }
   }
@@ -30,7 +34,7 @@ class OrderReturnModel {
     data['basket'] = basket.toString().toStringConversion();
     data['created_at'] = createdAt;
     data['order_items'] = orderItems.map((v) => v.toJson()).toList();
-      return data;
+    return data;
   }
 }
 
@@ -54,18 +58,18 @@ class OrderItems {
     productId = json['product_id'].toString().toInt();
     productName = json['product_name'].toString().toStringConversion();
     size = json['size'].toString().toStringConversion();
-    measurement = json['measurement'];
-    imageUrl = json['image_url'];
+    measurement = json['measurement'].toString().toStringConversion();
+    imageUrl = json['image_url'].toString().toStringConversion();
     unitTags = json['unit_tags'].cast<String>();
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data['product_id'] = productId;
-    data['product_name'] = productName;
-    data['size'] = size;
-    data['measurement'] = measurement;
-    data['image_url'] = imageUrl;
+    data['product_id'] = productId.toString().toInt();
+    data['product_name'] = productName.toString().toStringConversion();
+    data['size'] = size.toString().toStringConversion();
+    data['measurement'] = measurement.toString().toStringConversion();
+    data['image_url'] = imageUrl.toString().toStringConversion();
     data['unit_tags'] = unitTags;
     return data;
   }
--- lib/features/views/profile/provider/order_return_provider.dart
+++ lib/features/views/profile/provider/order_return_provider.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:packer/constants/app_urls.dart';
 import 'package:packer/controllers/api/dio_client.dart';
 import 'package:packer/controllers/services/api/enum/request_type.dart';
+import 'package:packer/features/views/order/models/order_return_model.dart';
 
 class OrderReturnProvider extends ChangeNotifier {
+
+   OrderReturnModel returnOrder= ;
   Future<void> fetchOrderReturns() async {
     final response = await DioClient().request(
       url: AppUrls.orderReturnUrl,
@@ -11,7 +14,8 @@ class OrderReturnProvider extends ChangeNotifier {
     );
     
     if (response.statusCode == 200) {
-      
+           returnOrder = data.map((order) => OrderNotification.fromJson(order)).toList();
+
       
     } else {
       throw Exception('Failed to load order returns');
