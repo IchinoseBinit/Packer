4cf17f08966ec9627c18c758e04a9c62ac08619d
Sujeet <sujeetchakra69@gmail.com>
2025-11-20 16:26:32 +0545

    style(ui): minor UI change

--- lib/features/views/packer_transfer/provider/packer_transfer_provider.dart
+++ lib/features/views/packer_transfer/provider/packer_transfer_provider.dart
@@ -837,9 +837,11 @@ class PackerTransferProvider extends ChangeNotifier {
         return;
       }
       final homeProvider = Provider.of<HomeProvider>(context, listen: false);
-      final url = homeProvider.isMainStore() == true
-          ? AppUrls.acceptTransferUrl
-          : AppUrls.completeTransferUrl;
+      final url =
+          //  homeProvider.isMainStore() == true
+          //     ? AppUrls.acceptTransferUrl
+          //     :
+          AppUrls.completeTransferUrl;
       final response = await DioClient().request(
           requestType: RequestType.postWithToken,
           url: url.replaceAll('id', id.toString()),
